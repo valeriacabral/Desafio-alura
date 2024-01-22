6f53bfe2974adbee7628815b40e260933e7f4560
Valéria Cabral <109596998+valeriacabral@users.noreply.github.com>
2024-01-21 23:55:48 -0300

    Add files via upload

---
+++ desafios-alura/app.js
@@ -0,0 +1,34 @@
+// Mostrar um alerta com a mensagem "Boas vindas ao nosso site!"
+alert('Boas vindas ao nosso site!');
+
+// Declarar uma variável chamada nome e atribuir a ela o valor "Lua"
+let nome = "Lua";
+
+// Criar uma variável chamada idade e atribuir a ela o valor 25
+let idade = 25;
+
+// Definir uma variável numeroDeVendas e atribuir a ela o valor 50
+let numeroDeVendas = 50;
+
+// Definir uma variável saldoDisponivel e atribuir a ela o valor 1000
+let saldoDisponivel = 1000;
+
+// Exibir um alerta com o texto "Erro! Preencha todos os campos"
+alert('Erro! Preencha todos os campos');
+
+// Declarar uma variável chamada mensagemDeErro e atribuir a ela o valor "Erro! Preencha todos os campos"
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+// Exibir um alerta com o valor da variável mensagemDeErro
+alert(mensagemDeErro);
+
+// Usar um prompt para perguntar o nome do usuário e armazená-lo na variável nome
+nome = prompt('Digite seu nome:');
+
+// Pedir ao usuário para digitar sua idade usando um prompt e armazená-la na variável idade
+idade = prompt('Digite sua idade:');
+
+// Agora, caso a idade seja maior ou igual a 18, exibir um alerta com a mensagem "Pode tirar a habilitação!"
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
